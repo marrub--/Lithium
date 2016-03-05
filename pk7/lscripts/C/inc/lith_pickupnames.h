1b4c858dfd795e16dd718dde0e366d981840f3c3
Marrub <marrub@greyserv.net>
2016-03-05 10:56:17 -0500

    Add a missing file. I forgot to commit this.

---
+++ pk7/lscripts/C/inc/lith_pickupnames.h
@@ -0,0 +1,41 @@
+pickups(weapon_pistol, {
+   "Postil",
+   null
+})
+
+pickups(weapon_shotgun, {
+   "Shtegnu",
+   null
+})
+
+pickups(weapon_supershotgun, {
+   "Sbepru Shtetgenu",
+   null
+})
+
+pickups(weapon_combatrifle, {
+   "Cmobta Riffel",
+   "Combit Raffle",
+   "Combat Rifl",
+   "Rifcom Litbl",
+   "Bltbm Liffem",
+   null
+})
+
+pickups(weapon_rocketlauncher, {
+   "Ruckebt Laurence",
+   null
+})
+
+pickups(weapon_plasmarifle, {
+   "Spalmo Rinfer",
+   null
+})
+
+pickups(weapon_bfg9000, {
+   "Biffgee Nintendo",
+   null
+})
+
+#undef pickups
+
