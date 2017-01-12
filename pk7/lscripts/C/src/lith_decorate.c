4bc0d72452b7b89ad937629bf91c97cd78093054
Graham <marrub@greyserv.net>
2017-01-12 15:04:22 -0500

    Move some functions from lith_decorate.c to lith_weapons.c

--- pk7/lscripts/C/src/lith_decorate.c
+++ pk7/lscripts/C/src/lith_decorate.c
@@ -1,86 +1,11 @@
 #include "lith_common.h"
 #include "lith_weapons.h"
 #include "lith_player.h"
-#include "lith_pickups.h"
 #include "lith_hudid.h"
 
 //----------------------------------------------------------------------------
 // Scripts
 //
-
-[[__call("ScriptI"), __address(14242), __extern("ACS")]]
-void Lith_WeaponPickup(int user_pickupparm, int user_tid)
-{
-   static __str pickupsounds[] = {
-      [weapon_unknown]  = "MMMMHMHMMMHMMM",
-      [weapon_pistol]   = "weapons/pistol/pickup",
-      [weapon_shotgun]  = "weapons/shotgun/pickup",
-      [weapon_rifle]    = "weapons/rifle/pickup",
-      [weapon_launcher] = "weapons/rocket/pickup",
-      [weapon_plasma]   = "weapons/plasma/pickup",
-      [weapon_bfg]      = "weapons/cannon/pickup"
-   };
-   
-   player_t *p = Lith_LocalPlayer;
-   
-   if(p->weapons & (1 << user_pickupparm))
-      return;
-   
-   p->weaponsheld++;
-   
-   if(!ACS_GetCVar("sv_weaponstay"))
-      ACS_Thing_Remove(user_tid);
-   
-   if(!p->upgrades[UPGR_7777777].active)
-      ACS_LocalAmbientSound(pickupsounds[user_pickupparm], 127);
-   else
-      ACS_LocalAmbientSound("marathon/pickup", 127);
-   
-   switch(user_pickupparm)
-   {
-   case weapon_pistol:   Lith_UnlockBIPPage(&p->bip, "Pistol");   break;
-   case weapon_shotgun:  Lith_UnlockBIPPage(&p->bip, "Shotgun");  break;
-   case weapon_rifle:    Lith_UnlockBIPPage(&p->bip, "Rifle");    break;
-   case weapon_launcher:
-      ACS_GiveInventory("Lith_RocketAmmo", 20);
-      Lith_UnlockBIPPage(&p->bip, "Launcher");
-      break;
-   case weapon_plasma:
-      ACS_GiveInventory("Lith_PlasmaAmmo", 2000);
-      Lith_UnlockBIPPage(&p->bip, "Plasma");
-      break;
-   case weapon_bfg:
-      ACS_GiveInventory("Lith_CannonAmmo", 2);
-      Lith_UnlockBIPPage(&p->bip, "Cannon");
-      break;
-   }
-   
-   if(ACS_GetCVar("lith_sv_stupidpickups"))
-      Lith_StupidPickup(p, user_pickupparm);
-   else
-      Lith_IntelligentPickup(p, user_pickupparm);
-}
-
-[[__call("ScriptS"), __extern("ACS")]]
-int Lith_CircleSpread(fixed mdx, fixed mdy, bool getpitch)
-{
-   static fixed A;
-   static fixed P;
-   
-   if(!getpitch)
-   {
-      fixed dx = RandomFixed(mdx, 0.0);
-      fixed dy = RandomFixed(mdy, 0.0);
-      fixed a  = RandomFixed(1.0, -1.0);
-      
-      A = sink(a) * dx;
-      P = cosk(a) * dy;
-      
-      return bitsk(A);
-   }
-   else
-      return bitsk(P);
-}
 
 [[__call("ScriptS"), __extern("ACS")]]
 int Lith_UniqueTID(void)
