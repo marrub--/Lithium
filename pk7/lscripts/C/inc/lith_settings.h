02d9406c5f9a6ae3a40298350fd244a822bb7e72
Graham <marrub@greyserv.net>
2017-01-17 15:18:47 -0500

    Upgrades::RifleModes: Make the scope on burst fire optional

--- pk7/lscripts/C/inc/lith_settings.h
+++ pk7/lscripts/C/inc/lith_settings.h
@@ -8,6 +8,7 @@ Category("Weapons")
 Float("Scope zoom factor", "lith_weapons_zoomfactor", 1.0f, 10.0f)
 Float("Scope opacity",  "lith_weapons_scopealpha", 0.0f, 1.0f)
 Float("Weapon opacity", "lith_weapons_alpha", 0.0f, 1.0f)
+Bool("Modal Rifle scope", "lith_weapons_riflescope")
 
 Category("Heads Up Display")
 Bool("Show score",   "lith_hud_showscore")
--- pk7/lscripts/C/src/lith_upgr_riflemodes.c
+++ pk7/lscripts/C/src/lith_upgr_riflemodes.c
@@ -19,7 +19,8 @@ void Upgr_RifleModes_Deactivate(player_t *p, upgrade_t *upgr)
 [[__call("ScriptS")]]
 void Upgr_RifleModes_Update(player_t *p, upgrade_t *upgr)
 {
-   if(p->weapontype == weapon_rifle && p->riflefiremode == rifle_firemode_burst)
+   if(ACS_GetUserCVar(p->number, "lith_weapons_riflescope") &&
+      p->weapontype == weapon_rifle && p->riflefiremode == rifle_firemode_burst)
    {
       ACS_Warp(p->cameratid, 0, 0, p->viewheight, 0,
                WARPF_NOCHECKPOSITION | WARPF_MOVEPTR |
