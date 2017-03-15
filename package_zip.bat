828e66fd702ca3684a33ecf9d53cbf3ba7701593
Graham <marrub.xz@gmail.com>
2017-03-15 02:38:35 -0400

    Give the charge fist a hit animation

--- package_zip.bat
+++ package_zip.bat
@@ -1,3 +1,3 @@
 cd pk7
-C:\stuff\utils\7z\7za.exe -tzip a ..\Lithium.pk3 .\* -xr!lcode\bin -xr!*.gitignore -xr!*.bat -xr!*.dbs -xr!*.ir
+C:\stuff\utils\7z\7za.exe -tzip a ..\Lithium.pk3 .\* -xr!lcode\bin -xr!*.gitignore -xr!*.bat -xr!*.dbs -xr!*.ir -xr!*.ba*k*
 
