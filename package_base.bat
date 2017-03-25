06f6cdc0640fa69fe39710012227d96d829663bb
Graham <marrub.xz@gmail.com>
2017-03-25 18:21:18 -0400

    Add standalone damage bob mod

--- package_base.bat
+++ package_base.bat
@@ -3,3 +3,6 @@ C:\stuff\utils\7z\7za.exe a %2 ..\Lithium.%1 .\* -xr!*.gitignore -xr!*.bat -xr!*
 
 cd ..\pk7_extras
 C:\stuff\utils\7z\7za.exe a %2 ..\Lithium_Extras.%1 .\*
+
+cd ..\pk7_damagebob
+C:\stuff\utils\7z\7za.exe a %2 ..\Lithium_DamageBob.%1 .\*
