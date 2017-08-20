0346b9c558e4e8ecd828fa1c53d6f52f3141028c
Marrub <marrub--@users.noreply.github.com>
2017-08-20 10:24:02 -0400

    Move stuff (again)

--- package_base.bat
+++ package_base.bat
@@ -1,5 +1,8 @@
 cd pk7
 C:\stuff\utils\7z\7za.exe a %2 ..\Lithium.%1 .\* -xr!*.gitignore -xr!*.bat -xr!*.dbs -xr!*.wad.b*
 
+cd pk7_cpk1
+C:\stuff\utils\7z\7za.exe a %2 ..\Lithium_ExtraClasses.%1 .\* -xr!*.gitignore -xr!*.bat -xr!*.dbs -xr!*.wad.b*
+
 cd ..\pk7_damagebob
 C:\stuff\utils\7z\7za.exe a %2 ..\Lithium_DamageBob.%1 .\*
