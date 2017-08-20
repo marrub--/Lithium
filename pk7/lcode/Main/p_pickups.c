34d210320a1dac2e99beb7ac878d61de07817471
Marrub <marrub--@users.noreply.github.com>
2017-08-19 20:33:29 -0400

    Move pickup text to LANGUAGE

--- pk7/lcode/Main/p_pickups.c
+++ pk7/lcode/Main/p_pickups.c
@@ -3,14 +3,7 @@
 
 #define name(p) {countof(p), p}
 
-struct pickupfmt_s
-{
-   int flag;
-   __str fmt;
-};
-
-struct pickupname_s
-{
+struct pickupname_s {
    size_t const num;
    __str *const ptr;
 };
@@ -215,135 +208,37 @@ static struct pickupname_s const pickupnames[weapon_max] = {
    [weapon_c_delear]   = name(names_c_delear),
 };
 
-// Flags
-// 1: Two copies of name
-// 2: Very rare
-// 4: Randomized certainty
-static struct pickupfmt_s const pickupfmt[] = {
-   0, "You got the %S!",
-   0, "Oh yes, the %S.",
-   0, "The %S has been acquired.",
-   0, "Snatched up a %S!",
-   0, "Oh baby, it's time for %S!",
-   0, "There was a %S here, but you stole it. It is now in your inventory. You Monster.",
-   0, "Acquired a %S.",
-   0, "The %S is safe and sound with you.",
-   0, "I bet that %S totally wanted you to pick it up.",
-   0, "It's not like the %S wanted you to pick it up or anything.",
-   0, "It's a %S... I think.",
-   4, "You thought you picked up a %S ... %S",
-   0, "1 in 3 %Ss do not approve of being picked up.",
-   0, "Studies show that normal people do not steal %Ss.",
-   0, "OH NO, NOT %S",
-   4, "What? Is that.. %S? ... %S",
-   0, "Magnetically attached a %S to your body, crushing your every bone.",
-   0, "DavidPH was here, nyu.",
-   0, "%S-senpai still won't notice you.",
-   0, "Grabbed a %S!",
-   0, "Snatched up a %S!",
-   0, "Embraced the %S carefully!",
-   0, "There's a thing that looks like a %S here. You picked it up. Past-tense.",
-   0, "Another day, another %S.",
-   0, "Xaser didn't make this %S, so it sucks. You should drop it.",
-   0, "\Cg%S. Finally.",
-   4, "The %S. %S",
-   0, "I don't know about you, but I quite like the %S. I'll pick it up for you.",
-   0, "...Huh? You do something? I wasn't paying attention.",
-   1, "The %S. It is very much a %S.",
-   0, "%S, desu.",
-   0, "?? ??? ???? ?? ????????? ???? ??",
-   1, "My %S lies over the ocean, my %S lies over the sea.",
-   0, "Liberated the %S from gypsies.",
-   0, "Granted the %S true independence.",
-   1, "The %S, for maximum %S-ing.",
-   4, "Brutal %S, featuring 5x more explosions! .. %S",
-   2, "aeiou",
-   0, "What is a man? A miserable pile of %Ss!",
-   0, "The %S was delicious.",
-   1, "There's plenty of %Ss in the sea, but you're the only %S for me.",
-   0, "Disassembled the %S and reassembled it into your inventory.",
-   0, "The %S is keepin' it real, as usual.",
-   0, "The %S finds you agreeable, and lets you pick it up.",
-   0, "You have attained true %S.",
-   2, "DRUGS",
-   0, ">%S >2012",
-   0, "TNT1 A 0 A_TakeInventory(\"%S\", 1)",
-   0, "Super rare %S acquired!",
-   0, "Sacked the %S!",
-   0, "The %S is your new waifu.",
-   0, "The %S is grateful for your presence.",
-   0, "The %S appears to be emitting hatred.",
-   0, "[%S intensifies]",
-   0, "Ah, yes, the %S.",
-   0, "Wait, the %S is resonating!",
-   0, "Oh, I guess it's an %S.",
-   0, "THIS IS SOME KIND OF %S",
-   0, "A perfectly preportioned, rare breed of %S appears. "
-      "Its natural predator, the Doomguy, picks it up.",
-   0, "%S version 7.0 installed.",
-   0, "Hazardous %S contained.",
-   0, "Warning: Dangerous %S levels detected.",
-   0, "Oh, yeah, I'm *sure* the %S will help.",
-   0, "YOU WOULDN'T DOWNLOAD A %S",
-   0, "Vital %S destroyed.",
-   0, "Explosive %S apparatus destroyed.",
-   0, "The %S shoots back at you, but you were stronger.",
-   0, "why are you reading this? eyes on the road, dumpass.",
-   2, "What the fuck did you just fucking say about %S, you little bitch?\n"
-      "I'll have you know I graduated top of my class in the Navy Seals,\n"
-      "and I've been involved in numerous secret raids on Al-Quaeda, and I\n"
-      "have over 300 confirmed kills.\n"
-      "I am trained in gorilla warfare and I'm the top sniper\n"
-      "in the entire US armed forces.\n"
-      "You are nothing to me but just another target. I will wipe you the\n"
-      "fuck out with precision the likes of which has never been seen before\n"
-      "on this Earth, mark my fucking words.\n"
-      "You think you can get away with saying that shit to me over the Internet?\n"
-      "Think again, fucker. As we speak I am contacting my secret network of\n"
-      "spies across the USA and your IP is being traced right now so you\n"
-      "better prepare for the storm, maggot.\n"
-      "The storm that wipes out the pathetic little thing you call your life.\n"
-      "You're fucking dead, kid.\n"
-      "I can be anywhere, anytime, and I can kill you in over\n"
-      "seven hundred ways, and that's just with my bare hands. Not only am I\n"
-      "extensively trained in unarmed combat, but I have access to the entire\n"
-      "arsenal of the United States Marine Corps and I will use it to its full\n"
-      "extent to wipe your miserable ass off the face of the continent, you little shit.\n"
-      "If only you could have known what unholy retribution your little \"clever\"\n"
-      "comment was about to bring down upon you, maybe you would have held\n"
-      "your fucking tongue. But you couldn't, you didn't, and now you're paying\n"
-      "the price, you goddamn idiot. I will shit fury all over you\n"
-      "and you will drown in it. You're fucking dead, kiddo.",
-};
-
-static __str uncertainty[] = {
-   "Yes.",
-   "Maybe.",
-   "Hmm, yes. Indeed.",
-   "Yeah.",
-   "Yep.",
-   "Ye.",
-   "Mmyes.",
-   "Yes. No. Yes?",
-   "Yes, no, maybe. I don't know."
-};
-
-static size_t const pickupfmtnum   = countof(pickupfmt);
-static size_t const uncertaintynum = countof(uncertainty);
-
 static void Lith_StupidPickup(player_t *p, int weapon)
 {
+   static __str uncertainty[] = {
+      "Yes.",
+      "Maybe.",
+      "Hmm, yes. Indeed.",
+      "Yeah.",
+      "Yep.",
+      "Ye.",
+      "Mmyes.",
+      "Yes. No. Yes?",
+      "Yes, no, maybe. I don't know."
+   };
+
+   static int fmtnum;
    struct pickupname_s const *names = &pickupnames[weapon];
 
-   int ifmt  = ACS_Random(0, pickupfmtnum   - 1);
-   int iname = ACS_Random(1, names->num     - 1);
-   int iunc  = ACS_Random(0, uncertaintynum - 1);
+   if(!fmtnum)
+      fmtnum = strtoi_str(Language("LITH_PICKUP_NUM"), null, 10);
 
-   if(pickupfmt[ifmt].flag & 2)
-      ifmt = ACS_Random(0, pickupfmtnum - 1);
+   int ifmt  = ACS_Random(0, fmtnum);
+   int iname = ACS_Random(1, names->num - 1);
+   int iunc  = ACS_Random(0, countof(uncertainty) - 1);
+   int flag  = strtoi_str(Language("LITH_PICKUP_%.3i_FLAGS", ifmt), null, 0);
 
-   __str fmt  = StrParam("> %S", pickupfmt[ifmt].fmt);
-   int   flag = pickupfmt[ifmt].flag;
+   if(flag & 2) {
+      ifmt = ACS_Random(0, fmtnum);
+      flag = strtoi_str(Language("LITH_PICKUP_%.3i_FLAGS", ifmt), null, 0);
+   }
+
+   __str fmt = StrParam("> %LS", StrParam("LITH_PICKUP_%.3i", ifmt));
 
    if(flag & 1 && flag & 4)
       p->log(fmt, names->ptr[iname], names->ptr[iname], uncertainty[iunc]);
