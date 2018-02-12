19d41132657c8690df5cbb6c3c372788695c5805
Marrub <marrub--@users.noreply.github.com>
2018-02-12 09:52:24 -0500

    make death particles more fancy with shaders

---
+++ pk7/lshaders/death.fp.glsl
@@ -0,0 +1,13 @@
+uniform float timer;
+
+vec4 Process(vec4 color)
+{
+   vec2 uv = gl_TexCoord[0].st;
+   float a = getTexel(uv).a;
+   float s = cos(sin(uv.x) * uv.y * 3 + timer) + 0.5;
+   float tt = (uv.x * sin(timer) + uv.y * cos(timer)) * 3;
+   vec4 fc = vec4(-(s * 0.05 * cos(tt)), 1, -(s * 0.05 * sin(tt)), a);
+   return fc * color;
+}
+
+// EOF
