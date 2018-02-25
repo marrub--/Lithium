ed2e345da8bddfde2120b51fb425376801f6d71c
Marrub <marrub--@users.noreply.github.com>
2018-02-25 05:01:39 -0500

    misc stuff for cyberspace

---
+++ source/Main/w_cyberspace.c
@@ -0,0 +1,27 @@
+// Copyright © 2018 Graham Sanderson, all rights reserved.
+#include "lith_common.h"
+#include "lith_file.h"
+#include "lith_token.h"
+#include "lith_tokbuf.h"
+
+//
+// Lith_CSLoadMap
+//
+[[__call("ScriptS"), __extern("ACS")]]
+void Lith_CSLoadMap(void)
+{
+   FILE *fp = W_Open("lfiles/Cyberspace.txt", c"r");
+   int w, h, *map;
+
+   __fscanf_str(fp, "%i %i", &w, &h);
+
+   map = Malloc(sizeof(int) * w * h);
+
+   for(int y = 0; y < h; y++) for(int x = 0; x < w; x++)
+      __fscanf_str(fp, "%i ", &map[x + y * w]);
+
+   fclose(fp);
+   Dalloc(map);
+}
+
+// EOF
