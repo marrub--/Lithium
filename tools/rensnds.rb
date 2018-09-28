0a88be3d2466d54798dcc995f78ea5b0823af1ea
Marrub <marrub.xz@gmail.com>
2018-09-28 05:31:55 -0400

    do the same for the sound files

---
+++ tools/rensnds.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+## Copyright © 2018 Graham Sanderson, all rights reserved.
+## RenSnds: Renames stuff in sndinfo files.
+## An input file can be generated with:
+## git diff --staged --name-status -C | sed '/R100/!d' | cut -f 2,3 | sed 's/pk7\///g'
+
+renames = Hash[open(ARGV[0], "rt").each_line.map{|l| l.chomp.split("\t", 2)}]
+
+`find pk7 -type f -name 'sndinfo.*'`.split("\n").each do |f|
+   s = open(f, "rb").read
+   renames.each{|k, v| s.sub! k, v}
+   open(f, "wb").write s
+end
+
+## EOF
