a44cc221e2072adca4cdc4165c340f43b77cdc0f
Marrub <marrub.xz@gmail.com>
2018-11-30 03:53:27 -0500

    add new dynamic array functions

--- source/Headers/lith_darray.h
+++ source/Headers/lith_darray.h
@@ -14,17 +14,25 @@
    __VA_ARGS__ type  *name##V; \
    __VA_ARGS__ size_t name##C, name##S
 
+#define Vec_GrowV(vec, n) \
+   ((vec##S) += (n), \
+    (vec##V) = Ralloc((vec##V), sizeof(*(vec##V)) * (vec##S)))
+
 #define Vec_GrowN(vec, n, g) \
    ((vec##C) + (n) > (vec##S) ? \
-      ((vec##S) += (n) + (g), \
-       (vec##V) = Ralloc((vec##V), sizeof(*(vec##V)) * (vec##S))) : \
-      (void *)0)
+    Vec_GrowV(vec, (n) + (g)) : \
+    (void *)0)
 
 #define Vec_Grow(vec, n) Vec_GrowN(vec, n, 8)
+
+#define Vec_Resize(vec, n) \
+   ((vec##C) = n, (vec##S) >= (vec##C) ? (void)0 : (void)Vec_GrowV(vec, n - (vec##S)))
 
 #define Vec_Next(vec) ((vec##V)[(vec##C)++])
 
 #define Vec_Clear(vec) \
    (Dalloc((vec##V)), (vec##V) = NULL, (vec##C) = (vec##S) = 0)
+
+#define Vec_MoveEnd(vec, i, l) (memmove(&(vec##V)[(vec##C)], i, l), (vec##C) += (l))
 
 #endif
