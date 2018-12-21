21458b59f64ea5da50e74e7554c8e3e9573028b7
Marrub <marrub.xz@gmail.com>
2018-12-21 07:19:45 -0500

    fix minor stuff

--- tools/gettf.sh
+++ tools/gettf.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env sh
-if [ -f tools/ttfuck ]
-then
-   rm -rf tools/ttfuck
-fi
-
+rm -rf tools/ttfuck
 mkdir tools/ttfuck
 wget http://mab.greyserv.net/f/ttfuck.7z
 7za x ttfuck.7z -otools/ttfuck
