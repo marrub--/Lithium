c4869149387e416a669dbe061ab6c1541549c886
Marrub <marrub.xz@gmail.com>
2018-12-24 19:10:53 -0500

    move old drawing stuff to its own file

---
+++ source/Main/m_draw.c
@@ -0,0 +1,91 @@
+// Copyright © 2016-2018 Alison Sanderson, all rights reserved.
+#include "lith_common.h"
+
+#include <stdio.h>
+
+StrEntOFF
+
+#define DrawSpriteBegin(name) \
+   do { \
+      while(name[0] == ':') \
+      { \
+         ACS_BeginPrint(); \
+         ACS_PrintLocalized(name); \
+         name = ACS_EndStrParam(); \
+      } \
+      ACS_SetFont(name); \
+      ACS_BeginPrint(); \
+      ACS_PrintChar('a'); \
+      ACS_MoreHudMessage(); \
+   } while(0)
+
+stkcall
+void DrawSprite(__str name, int flags, int id, fixed x, fixed y, fixed hold)
+{
+   DrawSpriteBegin(name);
+   ACS_OptHudMessage(flags|HUDMSG_NOTWITHFULLMAP, id, CR_UNTRANSLATED, x, y, hold);
+   ACS_EndHudMessage();
+}
+
+stkcall
+void DrawSpriteX(__str name, int flags, int id, fixed x, fixed y, fixed hold, fixed a1)
+{
+   DrawSpriteBegin(name);
+   ACS_OptHudMessage(flags|HUDMSG_NOTWITHFULLMAP, id, CR_UNTRANSLATED, x, y, hold);
+   ACS_EndHudMessage(a1);
+}
+
+stkcall
+void DrawSpriteXX(__str name, int flags, int id, fixed x, fixed y, fixed hold, fixed a1, fixed a2)
+{
+   DrawSpriteBegin(name);
+   ACS_OptHudMessage(flags|HUDMSG_NOTWITHFULLMAP, id, CR_UNTRANSLATED, x, y, hold);
+   ACS_EndHudMessage(a1, a2);
+}
+
+stkcall
+void DrawSpriteXXX(__str name, int flags, int id, fixed x, fixed y, fixed hold, fixed a1, fixed a2, fixed a3)
+{
+   DrawSpriteBegin(name);
+   ACS_OptHudMessage(flags|HUDMSG_NOTWITHFULLMAP, id, CR_UNTRANSLATED, x, y, hold);
+   ACS_EndHudMessage(a1, a2, a3);
+}
+
+stkcall
+void DrawSpritePlain(__str name, int id, fixed x, fixed y, fixed hold)
+{
+   DrawSpriteBegin(name);
+   ACS_OptHudMessage(HUDMSG_NOTWITHFULLMAP, id, CR_UNTRANSLATED, x, y, hold);
+   ACS_EndHudMessage();
+}
+
+stkcall
+void DrawSpriteAlpha(__str name, int id, fixed x, fixed y, fixed hold, fixed alpha)
+{
+   DrawSpriteBegin(name);
+   ACS_OptHudMessage(HUDMSG_ALPHA|HUDMSG_NOTWITHFULLMAP, id, CR_UNTRANSLATED, x, y, hold);
+   ACS_EndHudMessage(alpha);
+}
+
+stkcall
+void DrawSpriteFade(__str name, int id, fixed x, fixed y, fixed hold, fixed fadetime)
+{
+   DrawSpriteBegin(name);
+   ACS_OptHudMessage(HUDMSG_FADEOUT|HUDMSG_NOTWITHFULLMAP, id, CR_UNTRANSLATED, x, y, hold);
+   ACS_EndHudMessage(fadetime);
+}
+
+void HudMessage(__str fmt, ...)
+{
+   va_list vl;
+
+   ACS_BeginPrint();
+
+   va_start(vl, fmt);
+   __vnprintf_str(fmt, vl);
+   va_end(vl);
+
+   ACS_MoreHudMessage();
+}
+
+// EOF
