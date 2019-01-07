386750a5754a012e3eca9631f9a594acd33b1e7b
Marrub <marrub.xz@gmail.com>
2019-01-07 04:42:37 -0500

    optimize blood

--- tools/package.sh
+++ tools/package.sh
@@ -3,4 +3,4 @@
 ## By Alison Sanderson. Attribution is encouraged, though not required.
 ## See licenses/cc0.txt for more information.
 args='-xr!*.gitignore -xr!*.bat -xr!*.dbs -xr!*.wad.b* -tzip'
-(cd pk7 && 7za a ../Lithium.pk3 ./* ../licenses ../credits.txt $args)
+(cd pk7 && 7za a ../Lithium.pk3 ./* ../licenses ../credits.txt ../ir/lithmain_ld.txt $args)
