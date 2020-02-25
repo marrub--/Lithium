2c3f61fcf63151d1f931c35cfd8c788fb13ff3fc
Alison Watson <marrub.xz@gmail.com>
2020-02-24 19:08:09 -0500

    add new functionality to mkfont script

--- tools/gettf.sh
+++ tools/gettf.sh
@@ -7,3 +7,4 @@ mkdir tools/ttfuck
 wget http://mab.greyserv.net/f/ttfuck.7z
 7za x ttfuck.7z -otools/ttfuck
 rm ttfuck.7z
+chmod +x tools/ttfuck/ttfuck.exe
--- tools/mkfont.rb
+++ tools/mkfont.rb
@@ -17,11 +17,21 @@ FONTS=%w(MisakiG MisakiM JFDotG)
 CMAP=`tools/getcmap.rb`
 
 for f in FONTS
-   unless FileTest.exist? "tools/#{f}.ttf"
-      `wget 'http://mab.greyserv.net/f/#{f}.ttf' -O tools/#{f}.ttf`
-   end
+	unless FileTest.exist? "tools/#{f}.ttf"
+		`wget 'http://mab.greyserv.net/f/#{f}.ttf' -O tools/#{f}.ttf`
+	end
 
-   system %W(tools/ttfuck/ttfuck.exe tools/#{f}.ttf 8 #{CMAP} pk7/lfiles/Font_#{f}.txt pk7/lgfx/Font/#{f}).shelljoin
+	words = %W(tools/ttfuck/ttfuck.exe tools/#{f}.ttf 8 #{CMAP} pk7/lfiles/Font_#{f}.txt pk7/lgfx/Font/#{f})
+
+	`which mono`
+	words.prepend "mono" if $?.success?
+
+	system words.shelljoin
+
+	words = %W(advpng -z4)
+	words.append *(Dir.glob "pk7/lgfx/Font/#{f}/*.png")
+
+	system words.shelljoin
 end
 
 ## EOF
