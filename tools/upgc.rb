99a591172c5bc2462d376dcb19abbdc96ccf5fa4
Alison Watson <marrub.xz@gmail.com>
2020-02-29 00:04:03 -0500

    make upgc use new tokenizer

--- tools/upgc.rb
+++ tools/upgc.rb
@@ -14,25 +14,102 @@
 
 require_relative "corinth.rb"
 
-SCR = /[[:digit:]]/
-PRF = /[[:digit:]]/
-MUL = /-?\./
-GRP = /[[:alpha:]]/
+def parse_upgrade tks, tok
+   tok = tks.next.expect_after tok, :identi
+   nam = tok.text
 
-def reqs res
-   res[1...-1].split("|").each{|a| a.replace "UR_#{a}"}.join("|")
+   tok = tks.next.expect_after tok, :identi
+   inf = if tok.text == "N/A" then "0" else '"' + tok.text + '"' end
+
+   scr = tks.peek_or :number, "0"
+
+   pcl = []
+   loop do
+      tok = tks.next.expect_after tok, :identi
+      pcl.push tok.text
+      break unless tks.drop :bar
+   end
+   pcl = pcl.join " | "
+
+   flg = []
+   tok = tks.next.expect_after tok, :identi
+   for c, i in tok.text.chars.each_with_index
+         if c == "-"           then next
+      elsif c == "A" && i == 0 then flg.push c
+      elsif c == "D" && i == 1 then flg.push c
+      elsif c == "U" && i == 2 then flg.push c
+      elsif c == "R" && i == 3 then flg.push c
+      elsif c == "E" && i == 4 then flg.push c
+      else                          tok.raise_kw "funcs" end
+   end
+
+   prf = tks.peek_or :number, "0"
+
+   mul = tks.peek_or :period, "0" do |orig|
+      tok = tks.next.expect_after orig, :number
+      tok.text
+   end
+
+   grp = tks.peek_or :assign, "0" do |orig|
+      tok = tks.next.expect_after orig, :identi
+      "UG_" + tok.text
+   end
+
+   req = tks.peek_or :modulo, "0" do |orig|
+      req = []
+      tok = orig
+      loop do
+         tok = tks.next.expect_after tok, :identi
+         req.push "UR_" + tok.text
+         break unless tks.drop :bar
+      end
+      req.join " | "
+   end
+
+   {nam: nam, inf: inf, scr: scr, pcl: pcl, prf: prf, grp: grp, req: req,
+    mul: mul, flg: flg}
 end
 
-ifp, ofh = open(ARGV[0], "rt"), open(ARGV[1], "wt")
-ofc, off = open(ARGV[2], "wt"), open(ARGV[3], "wt")
+common_main do
+   tks = tokenize ARGV.shift
 
-ofh.puts <<_end_h_; ofc.puts <<_end_c_; off.puts <<_end_f_
+   upgrades = []
+   category = ""
+
+   loop do
+      tok = tks.next.expect_in_top [:colon, :plus, :eof]
+
+      case tok.type
+      when :colon
+         tok = tks.next.expect_after tok, :identi
+         category = "UC_" + tok.text
+      when :plus
+         upg = parse_upgrade tks, tok
+         upg[:cat] = category
+         upgrades.push upg
+      when :eof
+         break
+      end
+   end
+
+   open(ARGV.shift, "wt").puts <<_end_h_; open(ARGV.shift, "wt").puts <<_end_c_; open(ARGV.shift, "wt").puts <<_end_f_
 /* pk7/lzscript/Constants/u_names.zsc
  */
 #{generated_header "upgc"}
 
 enum /* UpgradeName */
 {
+#{
+res = String.new
+for upg in upgrades
+   res.concat "   UPGR_#{upg[:nam]},\n"
+end
+res.concat "   UPGR_BASE_MAX"
+res
+}
+};
+
+/* EOF */
 _end_h_
 #{generated_header "upgc"}
 
@@ -43,74 +120,37 @@ StrEntON
 /* Extern Objects ---------------------------------------------------------- */
 
 struct upgradeinfo const upgrinfobase[UPGR_BASE_MAX] = {
+#{
+res = String.new
+for upg in upgrades
+   res.concat %(   {{"#{upg[:nam]}", #{upg[:inf]}, #{upg[:scr]}}, #{upg[:pcl]}, #{upg[:cat]}, #{upg[:prf]}, #{upg[:grp]}, #{upg[:req]}, #{upg[:mul]}, UPGR_#{upg[:nam]}},\n)
+end
+res
+}
+};
+
+/* EOF */
 _end_c_
 #{generated_header "upgc"}
 
 #include "u_func_beg.h"
 
-_end_f_
-
-cat = String.new
-
-for ln in ifp
-   ln.chomp!
-   next if ln.empty?
-
-   res = ln[1..-1]
-   case ln[0]
-   when ":"; cat.replace "UC_#{res}"
-   when "+"
-      res = res.split
-      nam = res.shift
-      inf = res.shift
-      inf.replace(inf == "-" ? "0" : %("#{inf}"))
-      scr = if res[0][0] =~ SCR; res.shift else "0" end
-      pcl = res.shift
-      flg = []
-
-      for c, i in res.shift.chars.each_with_index
-            if c == "-";           next
-         elsif c == "A" && i == 0; flg << c
-         elsif c == "D" && i == 1; flg << c
-         elsif c == "U" && i == 2; flg << c
-         elsif c == "R" && i == 3; flg << c
-         elsif c == "E" && i == 4; flg << c
-         else raise "you done fucked up" end
-      end
-
-      prf = if res[0] && res[0][0] =~ PRF;       res.shift   else "0" end
-      mul = if res[0] && res[0]    =~ MUL;       res.shift   else "0" end
-      grp = if res[0] && res[0][0] =~ GRP; "UG_#{res.shift}" else "0" end
-      req = if res[0] && res[0][0] == "("; reqs  res.shift   else "0" end
-
-      ofh.puts "   UPGR_#{nam},"
-
-      ofc.puts <<_end_
-   {{"#{nam}", #{inf}, #{scr}}, #{pcl}, #{cat}, #{prf}, #{grp}, #{req}, #{mul}, UPGR_#{nam}},
-_end_
-
-      unless flg.empty?
-         off.puts <<_end_
-Case(#{nam})
-   #{flg.map{|a| "#{a}(#{nam})\n"}.join("   ")}
-_end_
-      end
+#{
+res = String.new
+for upg in upgrades
+   unless upg[:flg].empty?
+      nam = upg[:nam]
+      flg = upg[:flg].map do |a| "   #{a}(#{nam})" end.join "\n"
+      res.concat "Case(" + nam +")\n" + flg
    end
 end
+res
+}
 
-ofh.puts <<_end_h_; ofc.puts <<_end_c_; off.puts <<_end_f_
-   UPGR_BASE_MAX
-};
-
-/* EOF */
-_end_h_
-};
-
-/* EOF */
-_end_c_
 #include "u_func_end.h"
 
 /* EOF */
 _end_f_
+end
 
 ## EOF
