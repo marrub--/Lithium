c575cc4138f82a89552209df74d52d60c2f1b618
Alison Watson <marrub.xz@gmail.com>
2020-03-18 18:23:09 -0400

    add ammo mechanics for Thoth

--- source/u_headsupdist.c
+++ source/u_headsupdist.c
@@ -31,6 +31,22 @@ stkcall
 void Upgr_HeadsUpDisT_Render(struct player *p, struct upgrade *upgr)
 {
    Upgr_HeadsUpDisC_Render(p, upgr);
+
+   k32 manaperc = p->mana / (k32)p->manamax;
+
+   i32 hprc = ceilk(min(manaperc,       0.5) * 2 * 62);
+   i32 fprc = ceilk(max(manaperc - 0.5, 0.0) * 2 * 62);
+
+   PrintSprite(sp_HUD_C_MagicIcon, 1,1, 213,2);
+   PrintSprite(sp_HUD_C_BarSmall2, 1,1, 220,2);
+
+   SetClip(2, 219-5, hprc, 5);
+   PrintSprite(sp_HUD_C_ManaBar1, 2,1, 219,2);
+   ClearClip();
+
+   SetClip(2, 219-5, fprc, 5);
+   PrintSprite(sp_HUD_C_ManaBar2, 2,1, 219,2);
+   ClearClip();
 }
 
 /* EOF */
