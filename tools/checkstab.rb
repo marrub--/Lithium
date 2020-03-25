c8ae2c01d30a2b52205783d0de433443d69fa2e7
Alison Watson <marrub.xz@gmail.com>
2020-03-25 05:10:59 -0400

    add tool for checking string table

---
+++ tools/checkstab.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+## ---------------------------------------------------------------------------|
+##
+## Distributed under the CC0 public domain license.
+## By Alison Sanderson. Attribution is encouraged, though not required.
+## See licenses/cc0.txt for more information.
+##
+## ---------------------------------------------------------------------------|
+##
+## Checks the string table for duplicates and unused strings.
+##
+## ---------------------------------------------------------------------------|
+
+require 'set'
+
+names = Set[]
+
+open("source/include/m_stab.h").each_line do |ln|
+   if ln =~ /^X\((\w+),.+\)/
+      name = $~[1]
+      raise "double definition of #{name}" if names.include? name
+      names.add name
+   end
+end
+
+used = Set[]
+
+Dir.glob("source/**/*.{c,h}").each do |f|
+   next if f == "source/include/m_stab.h"
+   s = open(f).read
+   for name in names
+      if name =~ /sc_(.+)/
+         used.add name if s =~ /#{$~[1]}\b/
+      else
+         used.add name if s =~ /\b#{name}\b/
+      end
+   end
+end
+
+diff = names - used
+raise diff.to_a.join "\n" unless diff.empty?
+
+## EOF
