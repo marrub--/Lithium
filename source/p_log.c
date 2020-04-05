3d5dee376435679cf68e2a173a5d56fdaae94f59
Alison Watson <marrub.xz@gmail.com>
2020-04-05 03:27:21 -0600

    make the log actually show big messages

--- source/p_log.c
+++ source/p_log.c
@@ -20,8 +20,7 @@
 
 /* Static Functions -------------------------------------------------------- */
 
-static void LogV(i32 levl)
-{
+static void LogV(i32 levl) {
    ACS_BeginPrint();
 
    if(levl) {
@@ -30,14 +29,12 @@ static void LogV(i32 levl)
    }
 }
 
-static void LogPop(struct player *p)
-{
+static void LogPop(struct player *p) {
    p->log.hudC--;
    memmove(&p->log.hudV[0], &p->log.hudV[1], sizeof p->log.hudV[0] * p->log.hudC);
 }
 
-static void LogH(struct player *p, struct logdat *ld)
-{
+static void LogH(struct player *p, struct logdat *ld) {
    ld->ftim = 5;
    ld->time = 140 - p->log.curtime;
    p->log.curtime = 140;
@@ -46,16 +43,14 @@ static void LogH(struct player *p, struct logdat *ld)
    p->log.hudV[p->log.hudC++] = *ld;
 }
 
-static void LogF(struct player *p, struct logfdt *lf)
-{
+static void LogF(struct player *p, struct logfdt *lf) {
    Vec_GrowN(p->log.curmap->data, 1, 8);
    Vec_Next(p->log.curmap->data) = *lf;
 }
 
 /* Extern Functions -------------------------------------------------------- */
 
-void P_Log_Both(struct player *p, i32 levl, cstr fmt, ...)
-{
+void P_Log_Both(struct player *p, i32 levl, cstr fmt, ...) {
    struct logdat ld = {};
 
    LogV(levl);
@@ -71,8 +66,7 @@ void P_Log_Both(struct player *p, i32 levl, cstr fmt, ...)
    LogH(p, &ld);
 }
 
-void P_Log_HUDs(struct player *p, i32 levl, cstr fmt, ...)
-{
+void P_Log_HUDs(struct player *p, i32 levl, cstr fmt, ...) {
    struct logdat ld = {};
 
    LogV(levl);
@@ -87,8 +81,7 @@ void P_Log_HUDs(struct player *p, i32 levl, cstr fmt, ...)
    LogH(p, &ld);
 }
 
-void P_Log_Full(struct player *p, cstr fmt, ...)
-{
+void P_Log_Full(struct player *p, cstr fmt, ...) {
    struct logfdt lf = {};
 
    ACS_BeginPrint();
@@ -103,8 +96,7 @@ void P_Log_Full(struct player *p, cstr fmt, ...)
    LogF(p, &lf);
 }
 
-void P_Log_Entry(struct player *p)
-{
+void P_Log_Entry(struct player *p) {
    struct logmap *lm = nil;
    i32 lnum = MapNum;
 
@@ -121,29 +113,27 @@ void P_Log_Entry(struct player *p)
 
    p->log.curmap = lm;
 
-   p->logF(LC(LANG "ENTER_FMT"), lm->name, CanonTime(ct_full));
+   p->logF(LC(LANG "ENTER_FMT"), lm->name, CanonTime(ct_full, ticks));
 }
 
-script
-void P_Log_PTick(struct player *p)
-{
-   if(p->log.curtime == 0)
-   {
-      if(p->log.hudC)
-      {
+script void P_Log_PTick(struct player *p) {
+   if(p->log.curtime == 0) {
+      if(p->log.hudC) {
          LogPop(p);
          p->log.curtime = p->log.hudV[p->log.hudC - 1].time;
       }
-   }
-   else
+   } else {
       p->log.curtime--;
+   }
 
-   for(i32 i = 0; i < p->log.hudC; i++)
-      if(p->log.hudV[i].ftim) p->log.hudV[i].ftim--;
+   for(i32 i = 0; i < p->log.hudC; i++) {
+      if(p->log.hudV[i].ftim) {
+         p->log.hudV[i].ftim--;
+      }
+   }
 }
 
-void P_CBI_TabLog(struct gui_state *g, struct player *p)
-{
+void P_CBI_TabLog(struct gui_state *g, struct player *p) {
    static i32 const ht = 10;
 
    if(G_Button(g, .x = 25, 38, Pre(btnprev)))
@@ -158,8 +148,7 @@ void P_CBI_TabLog(struct gui_state *g, struct player *p)
 
    G_ScrollBegin(g, &CBIState(g)->logscr, 15, 50, 280, 175, lm->dataC * ht);
 
-   for(i32 i = 0; i < lm->dataC; i++)
-   {
+   for(i32 i = 0; i < lm->dataC; i++) {
       i32 const y = ht * i;
 
       if(G_ScrollOcclude(g, &CBIState(g)->logscr, y, ht)) continue;
@@ -171,19 +160,19 @@ void P_CBI_TabLog(struct gui_state *g, struct player *p)
    G_ScrollEnd(g, &CBIState(g)->logscr);
 }
 
-script
-void HUD_Log(struct player *p, i32 cr, i32 x, i32 yy)
-{
-   if(p->getCVarI(sc_hud_showlog))
-   {
+script void HUD_Log(struct player *p, i32 cr, i32 x, i32 yy) {
+   if(p->getCVarI(sc_hud_showlog)) {
       i32 yo;
+      i32 xs, ys;
 
-      if(p->getCVarI(sc_hud_logbig)) {yo = 200; SetSize(320, 240);}
-      else                           {yo = 255; SetSize(480, 300);}
+      if(p->getCVarI(sc_hud_logbig)) {yo = 200; xs = 320; ys = 240;}
+      else                           {yo = 255; xs = 480; ys = 300;}
+
+      SetSize(xs, ys);
+      SetClipW(0, 0, xs, ys, xs);
 
       i32 i = 0;
-      for(i32 i = 0; i < p->log.hudC; i++)
-      {
+      for(i32 i = 0; i < p->log.hudC; i++) {
          struct logdat const *const ld = &p->log.hudV[i];
 
          i32 y = 10 * i;
@@ -196,8 +185,7 @@ void HUD_Log(struct player *p, i32 cr, i32 x, i32 yy)
 
          if(ld->ftim) SetFade(fid_logadS + i, 1, 8);
 
-         if(CheckFade(fid_logadS + i))
-         {
+         if(CheckFade(fid_logadS + i)) {
             cstr s = RemoveTextColors_str(ld->inf, ACS_StrLen(ld->inf));
             PrintTextChS(s);
             PrintTextFX(s_lmidfont, CR_WHITE, x,1, y,ya, fid_logadS + i, ptf_add);
@@ -211,8 +199,7 @@ void HUD_Log(struct player *p, i32 cr, i32 x, i32 yy)
 /* Scripts ----------------------------------------------------------------- */
 
 script_str ext("ACS") addr("Lith_LogS")
-void Sc_Log(i32 levl, i32 type)
-{
+void Sc_Log(i32 levl, i32 type) {
    str name = ServCallS(sm_GetLogName);
 
    if(name[0] == '_') name = Language(LANG "LOG%S", name);
