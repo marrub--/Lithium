27cbee1b312bc4a4162d1a58b4e636588602ac4b
marrub <marrub.xz@gmail.com>
2023-11-19 01:39:48 -0700

    add generated lbigfont characters, replace newconsolefont

--- tools/mkfont.rb
+++ tools/mkfont.rb
@@ -4,15 +4,39 @@ require_relative "corinth.rb"
 require "set"
 require "rmagick"
 
-chrmap = Set[]
-open("pk7/language.txt").each_char do |c| chrmap.add c end
-chrmap -= ("\uE000".."\uEFFF").entries
-chrmap.delete " "
-chrmap.delete "\n"
-chrmap.delete "\r"
+def flatten_chrmap areas
+   areas = areas.flat_map do |area|
+      if area.kind_of? Range
+         area.entries.map do |n|
+            if n.kind_of? Numeric
+               n.chr(Encoding::UTF_8)
+            else
+               n
+            end
+         end
+      else
+         area
+      end
+   end
+   Set.new(areas)
+end
 
-def new_font name, &p
-   fnt = {name: name, dir: "pk7/fonts/#{name}"}
+def add_file_chars chrmap, fname
+   open(fname).each_char do |c| chrmap.add c end
+end
+
+CHRMAP_ASCII = flatten_chrmap [0x20..0x7E]
+CHRMAP_EXTND = flatten_chrmap [*CHRMAP_ASCII, 0x80..0x2E7F]
+CHRMAP = Set[]
+add_file_chars CHRMAP, "pk7/language.txt"
+add_file_chars CHRMAP, "source/debug.c"
+CHRMAP.subtract "\uE000".."\uEFFF"
+CHRMAP.delete " "
+CHRMAP.delete "\n"
+CHRMAP.delete "\r"
+
+def new_font name, cm = CHRMAP, &p
+   fnt = {name: name, chrmap: cm, dir: "pk7/fonts/#{name}"}
    fnt.instance_exec(&p)
    fnt
 end
@@ -25,25 +49,28 @@ def escape_text_character ch
    end
 end
 
-def make_character_image ptsize, font, ch, &blk
+def make_character_image ptsize, font, ch, x_size = 0, y_size = 0, &blk
    dr = Magick::Draw.new
    dr.pointsize = ptsize
    dr.font = font
    dr.fill = "white"
    dr.gravity = Magick::SouthWestGravity
-   txt_chr = escape_text_character ch
-   metr = dr.get_type_metrics txt_chr
+   tch = escape_text_character ch
+   metr = dr.get_type_metrics tch
    return nil if metr.bounds.x1.truncate == metr.bounds.x2.truncate
-   im = Magick::Image.new metr.max_advance, metr.ascent - metr.descent do |ii|
+   x_size = if x_size.respond_to? :call then x_size.call metr
+            else metr.bounds.x2.truncate + x_size end
+   y_size = if y_size.respond_to? :call then y_size.call metr
+            else metr.ascent - metr.descent + y_size end
+   im = Magick::Image.new x_size, y_size do |ii|
       ii.background_color = "none"
       ii.image_type = Magick::GrayscaleAlphaType
       ii.depth = 8
    end
    if blk
-      blk.call dr, im, txt_chr
+      blk.call dr, im, tch
    else
-      dr.text 0, 0, txt_chr
-      dr.draw im
+      dr_text dr, im, tch
    end
    im
 end
@@ -54,49 +81,83 @@ def push_image il, im, fl, ch
 end
 
 def write_files il, fl
-   il.write "#{self[:dir]}/%d.png"
+   il.write "#{self[:dir]}/temp_%d.png"
    for f, i in fl.each_with_index
-      File.rename "#{self[:dir]}/#{i}.png", "#{self[:dir]}/#{f}.png"
+      File.rename "#{self[:dir]}/temp_#{i}.png", "#{self[:dir]}/#{f}.png"
    end
 end
 
-def do_stroke dr, im, txt_chr
-   dr.text 0, 0, txt_chr
+def dr_text dr, im, tch, x = 0, y = 0
+   dr.text x, y, tch
    dr.draw im
+end
+
+def dr_stroke dr, im, tch
+   dr_text dr, im, tch
    dr.stroke = "none"
    dr.stroke_width = 0
-   dr.text 0, 0, txt_chr
-   dr.draw im
+   dr_text dr, im, tch
 end
 
 def all_files_glob
    "#{self[:dir]}/*.png"
 end
 
-def delete_all_files
-   File.delete(*Dir.glob(all_files_glob))
+def delete_files glob
+   File.delete(*Dir.glob(glob))
 end
 
-def call_optipng
+def call_optipng glob
    system "optipng",
    "-strip", "all",
    "-preserve", "-clobber",
-   "-o", "2", *Dir.glob(all_files_glob)
+   "-o", "2", *Dir.glob(glob)
 end
 
-jsmlfont = new_font "ljsmlfnt" do
+@jbigfont = new_font("lbigfont", CHRMAP - CHRMAP_EXTND) do
+   il = Magick::ImageList.new
+   fl = []
+   glob = "#{self[:dir]}/0000*.png"
+   self[:start_fn] = proc do delete_files glob end
+   self[:char_fn] = proc do |ch|
+      im = make_character_image(12, "working/x12y12pxMaruMinya.ttf", ch, 0, proc do |metr|
+         metr.ascent - metr.descent - 1
+      end)
+      if im
+         out = Magick::Image.new im.columns + 2, im.rows + 2 do |ii|
+            ii.background_color = "none"
+         end
+         im_cr = im.colorize 1, 1, 1, "#005500"
+         out = out.composite im_cr, -1+1, -1+1, Magick::SrcOverCompositeOp
+         out = out.composite im_cr, -1+1,  1+1, Magick::SrcOverCompositeOp
+         out = out.composite im_cr,  1+1, -1+1, Magick::SrcOverCompositeOp
+         out = out.composite im_cr,  1+1,  1+1, Magick::SrcOverCompositeOp
+         im_cr = im.colorize 1, 1, 1, "#007300"
+         out = out.composite im_cr, -1+1,  0+1, Magick::SrcOverCompositeOp
+         out = out.composite im_cr,  1+1,  0+1, Magick::SrcOverCompositeOp
+         out = out.composite im_cr,  0+1, -1+1, Magick::SrcOverCompositeOp
+         out = out.composite im_cr,  0+1,  1+1, Magick::SrcOverCompositeOp
+         im_cr = im.colorize 1, 1, 1, "#00ff00"
+         out = out.composite im_cr, 0+1, 0+1, Magick::SrcOverCompositeOp
+         push_image il, out, fl, ch
+      end
+   end
+   self[:end_fn] = proc do
+      write_files il, fl
+      call_optipng glob
+   end
+end
+@jsmlfont = new_font "ljsmlfnt" do
    il = Magick::ImageList.new
    fl = []
    grad = Magick::GradientFill.new 0, 0, 16, 0, "white", "#696969"
-   grad = Magick::Image.new 32, 16, grad
-   self[:start_fn] = proc do delete_all_files end
+   grad = Magick::Image.new 32, 16, grad do |ii|
+      ii.image_type = Magick::GrayscaleAlphaType
+      ii.depth = 8
+   end
+   self[:start_fn] = proc do delete_files all_files_glob end
    self[:char_fn] = proc do |ch|
-      im = make_character_image 16, "working/x12y16pxMaruMonica.ttf", ch do |dr, im, txt_chr|
-         dr.text 0, 0, txt_chr
-         dr.draw im
-         im.define "trim:edges", "east"
-         im.trim!
-      end
+      im = make_character_image 16, "working/x12y16pxMaruMonica.ttf", ch
       if im
          out = im.composite grad, 0, 0, Magick::InCompositeOp
          out = im
@@ -109,18 +170,20 @@ jsmlfont = new_font "ljsmlfnt" do
    end
    self[:end_fn] = proc do
       write_files il, fl
-      call_optipng
+      call_optipng all_files_glob
    end
 end
-jtrmfont = new_font "ltrmfont" do
+@jtrmfont = new_font "ltrmfont" do
    il = Magick::ImageList.new
    fl = []
-   self[:start_fn] = proc do delete_all_files end
+   self[:start_fn] = proc do delete_files all_files_glob end
    self[:char_fn] = proc do |ch|
-      im = make_character_image 11, "working/ipaexg.ttf", ch do |dr, im, txt_chr|
+      im = make_character_image(11, "working/ipaexg.ttf", ch, proc do
+         14
+      end) do |dr, im, tch|
          dr.stroke = "black"
          dr.stroke_width = 1
-         do_stroke dr, im, txt_chr
+         dr_stroke dr, im, tch
       end
       push_image il, im, fl, ch if im
    end
@@ -128,45 +191,48 @@ jtrmfont = new_font "ltrmfont" do
       write_files il, fl
    end
 end
-ltrmfont = new_font "ltrmfont" do
+@ltrmfont = new_font "ltrmfont" do
    il = Magick::ImageList.new
    fl = []
    self[:start_fn] = proc do end
    self[:char_fn] = proc do |ch|
-      im = make_character_image 11, "working/courierprime.ttf", ch do |dr, im, txt_chr|
+      im = make_character_image(11, "working/courierprime.ttf", ch, proc do
+         7
+      end) do |dr, im, tch|
          dr.stroke = "black"
          dr.stroke_width = 1
-         do_stroke dr, im, txt_chr
+         dr_stroke dr, im, tch
       end
       push_image il, im, fl, ch if im
    end
    self[:end_fn] = proc do
       write_files il, fl
-      call_optipng
+      call_optipng all_files_glob
    end
 end
-areaname = new_font "lareanam" do
+@areaname = new_font "lareanam", CHRMAP_ASCII do
    il = Magick::ImageList.new
    fl = []
-   self[:start_fn] = proc do delete_all_files end
+   self[:start_fn] = proc do delete_files all_files_glob end
    self[:char_fn] = proc do |ch|
-      im = make_character_image 30, "working/smodgui.ttf", ch do |dr, im, txt_chr|
+      im = make_character_image 30, "working/smodgui.ttf", ch, 6 do |dr, im, tch|
          dr.stroke = "black"
          dr.stroke_width = 5
-         do_stroke dr, im, txt_chr
-         im.define "trim:edges", "east"
-         im.trim!
+         dr_stroke dr, im, tch
       end
       push_image il, im, fl, ch if im
    end
    self[:end_fn] = proc do
       write_files il, fl
-      call_optipng
+      call_optipng all_files_glob
    end
 end
-[jsmlfont, jtrmfont, ltrmfont, areaname].each do |fnt|
+fontlist = ARGV.map do |v| instance_variable_get :"@#{v}" end
+fontlist =
+   [@jbigfont, @jsmlfont, @jtrmfont, @ltrmfont, @areaname] if fontlist.empty?
+fontlist.each do |fnt|
    fnt[:start_fn].call
-   chrmap.each do |ch| fnt[:char_fn].call(ch) end
+   fnt[:chrmap].each do |ch| fnt[:char_fn].call(ch) end
    fnt[:end_fn].call
 end
 
