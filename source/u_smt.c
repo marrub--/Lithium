71156cbcb3e031c40916ef76c1552d44f40c3d18
marrub <marrub.xz@gmail.com>
2023-12-26 20:47:48 -0700

    add basic test pages

--- source/u_smt.c
+++ source/u_smt.c
@@ -7,29 +7,41 @@ void Z_TriggerNegotiation(void) {
    if(!m) {
       return;
    }
+   enum {
+      negotiation_boss,
+      negotiation_kid,
+      negotiation_blackguard,
+      negotiation_prince,
+      negotiation_dude,
+   };
    switch(m->mi->type) {
    case mtype_mastermind:
    case mtype_cyberdemon:
    case mtype_phantom:
-      pl.dlg.page = 0;
+      pl.dlg.page = negotiation_boss;
       break;
    case mtype_zombie:
    case mtype_lostsoul:
-      pl.dlg.page = 1;
-      break;
-   default:
-   case mtype_zombiesg:
-   case mtype_zombiecg:
-   case mtype_imp:
-   case mtype_demon:
-   case mtype_mancubus:
    case mtype_arachnotron:
-   case mtype_cacodemon:
+      pl.dlg.page = negotiation_kid;
+      break;
+   case mtype_zombiesg:
+   case mtype_imp:
    case mtype_hellknight:
-   case mtype_baron:
    case mtype_revenant:
+      pl.dlg.page = negotiation_blackguard;
+      break;
+   case mtype_baron:
    case mtype_painelemental:
    case mtype_archvile:
+      pl.dlg.page = negotiation_prince;
+      break;
+   case mtype_zombiecg:
+   case mtype_mancubus:
+   case mtype_cacodemon:
+   case mtype_demon:
+      pl.dlg.page = negotiation_dude;
+      break;
    case mtype_angel:
    case mtype_darkone:
    case mtype_person:
